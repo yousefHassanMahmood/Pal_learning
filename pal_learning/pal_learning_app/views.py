1f373afda203709478671f6e8962e0f1175bbf44
Mosab Abdallah <abdallamosab083@gmail.com>
2025-07-10 18:29:20 +0800

    Added login and signup views

--- pal_learning/pal_learning_app/views.py
+++ pal_learning/pal_learning_app/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .models import CustomUser
 
-# Create your views here.
+def signup_view(request):
+    if request.method == "POST":
+        errors = CustomUser.objects.user_validator(request.POST)
+        if errors:
+            for msg in errors.values():
+                messages.error(request, msg)
+            return redirect('signup')
+
+        user = CustomUser.objects.create_user(
+            username=request.POST['email'],  
+            email=request.POST['email'],
+            first_name=request.POST['first_name'],
+            last_name=request.POST['last_name'],
+            address=request.POST['address'],
+            password=request.POST['password'],
+            role=request.POST.get('role', CustomUser.STUDENT)
+        )
+        login(request, user)
+        return redirect('home')  
+
+    return render(request, 'signup.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        errors = CustomUser.objects.login_validator(request.POST)
+        if errors:
+            for msg in errors.values():
+                messages.error(request, msg)
+            return redirect('login')
+
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, username=email, password=password)
+        if user:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Email or password incorrect.")
+            return redirect('login')
+
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
